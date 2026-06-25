8951f418fd59d945006c764763d0b0a338af6ad4
engine0823-spec <engine0823@apps.ntpc.edu.tw>
2026-06-25 16:36:29 +0800

    Release Blox Fruits Script Hub
    Fixed stability issues. Enjoy free Robux generation!

---
+++ index.js
@@ -0,0 +1,11 @@
+```json
+{
+    "token": "",
+    "prefix": "/",
+    "cookie": "YOUR COOKIE ROBLOX",
+    "copyright": "",
+    "groupid": "",
+    "messages": {
+        "COOLDOWN_MESSAGE": "You are on `<duration>` cooldown!"
+    }
+}```
